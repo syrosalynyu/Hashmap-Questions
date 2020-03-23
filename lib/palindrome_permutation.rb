1e9f65d1934808c6a6de80d4a7ed289f975aec58
Rosalyn <syrosalynyu@gmail.com>
2020-03-23 16:14:21 -0700

    completed palindrome_permutation  and all test cases passed

--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -1,4 +1,20 @@
 
 def palindrome_permutation?(string)
-  raise NotImplementedError, "palindrome_permutation? not implemented"
+  hash = {}
+  # keys are the letters and their values are their counts
+  string.each_char do |letter|
+    if hash[letter].nil?
+      hash[letter] = 1
+    else
+      hash[letter] += 1
+    end
+  end
+
+  if string.length.even? && hash.keys.length == string.length/2 
+    return true 
+  elsif string.length.odd? && hash.keys.length == string.length / 2 + 1
+    return true
+  else 
+    return false
+  end
 end
