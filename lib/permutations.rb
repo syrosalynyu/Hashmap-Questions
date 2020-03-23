a70d54264a068edd53d406104fa017f059778c7b
Rosalyn <syrosalynyu@gmail.com>
2020-03-23 16:12:27 -0700

    completed permutations and all test cases passed

--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,4 +1,24 @@
 
 def permutations?(string1, string2)
-  raise NotImplementedError, "permutations? not implemented"
+  hash = {}
+  string1.each_char do |letter|
+    if hash[letter].nil?
+      hash[letter] = 1
+    else
+      hash[letter] += 1
+    end
+  end
+
+  string2.each_char do |letter|
+    # if letter is not exist in hash => return false
+    if hash[letter].nil?
+      return false
+    else
+      hash[letter] -= 1
+    end
+  end
+ 
+  # if there are values that is not equal to 0 => return false; else return true
+  hash.values.all? {|value| value == 0} ? true : false
+  
 end
